c8546409e1f3989ec1778ba0c48a23b7ba0b97d4
Anh-vu <anhuvdinh@gmail.com>
2021-07-25 23:10:08 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name = "Vu"){
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language)
+{
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript")
+{
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introduction());
+console.log(introductionWithLanguage());
+console.log(introductionWithLanguageOptional());
